1a294e92f9d6d46cdc2bea5d6760ec2fd401b821
LazyKitty <wenzhao5105@163.com>
2026-01-18 20:11:06 +0800

    增加注释

--- src/utils/config_loader/_get_value.py
+++ src/utils/config_loader/_get_value.py
@@ -1,19 +1,38 @@
 import os
 from typing import Any, Mapping
 
+
 class Miss_env_exception(Exception):
+    """
+    缺少环境变量的自定义异常
+    """
     def __init__(self, key: str, *args: object) -> None:
         self.key = key
         self.message = f"Environment variable '{key}' is missing."
         super().__init__(self.message, *args)
 
 class Miss_key_exception(Exception):
+    """
+    字典缺少键的自定义异常
+    """
     def __init__(self, key: str):
         self.key = key
         self.message = f"Key '{key}' is missing."
         super().__init__(self.message)
 
 def get_value_from_env(key: str) -> str | bool:
+    """
+    从环境变量中获取值,解析字符串返回字符串或布尔值
+    
+    Args:
+        key: 指定要获取的环境变量的名
+    
+    Returns:
+        若字符串小写为 true 或 false, 则返回布尔值, 否则返回获取的值的字符串形式
+    
+    Raises:
+        Miss_env_exception: 没有该环境变量
+    """
     value = os.getenv(key)
     if value is None:
         raise Miss_env_exception(key)
@@ -27,19 +46,50 @@ def get_value_from_env(key: str) -> str | bool:
         return value
     
 def get_value_from_dict(config: Mapping[str, Any], key: str) -> Any:
+    """
+    从字典中键对应的值
+
+    Args:
+        config: 传入的字典对象
+        key: 要获取的键
+    
+    Returns:
+        字典中键对应的值
+
+    Raises:
+        Miss_key_exception: 字典对象中没有该键
+    """
     if key not in config:
         raise Miss_key_exception(key)
     return config[key]
 
-def parse_bool(value: str) -> bool:
+def parse_bool(
+        value: str, 
+        true_bools: tuple[str, ...] = ("true", "1", "yes", "y", "on"),
+        false_bools: tuple[str, ...] = ("false", "0", "no", "n", "off")
+    ) -> bool:
+    """
+    从字符串中解析是否为布尔值
+
+    Atgs:
+        value: 要解析的字符串
+        true_bools: 判定为 True 的字符串元组
+        false_bools: 判定为 False 的字符串元组
+
+    returns:
+        True | False
+    
+    Raises:
+        ValueError: 字符串不被判定为 True 或 False
+    """
     if isinstance(value, bool):
         return value
 
     value = value.strip().lower()
 
-    if value in ("true", "1", "yes", "y", "on"):
+    if value in true_bools:
         return True
-    if value in ("false", "0", "no", "n", "off"):
+    elif value in false_bools:
         return False
 
     raise ValueError(f"Invalid boolean value: {value}")
--- src/utils/config_loader/load_all_configs.py
+++ src/utils/config_loader/load_all_configs.py
@@ -6,15 +6,44 @@ from .load_config import Config, load_config
 from .load_env_config import Env_config, load_env_config
 from .load_trading_config import Trading_config, load_trading_config
 
+def check_file_exist(
+        dotenv_path: str=".env", 
+        config_path: str="config.yaml", 
+        trading_path: str="trading_config.yaml"
+    ):
+    """
+    检查配置文件是否缺失
 
-def load_all_configs(dotenv_path: str=".env") -> Tuple[Env_config, Config, Trading_config]:
-    # 检查 env, config, trading_config 是否存在
+    Args:
+        dotenv_path: .env 文件的路径
+        config_path: 配置文件路径
+        trading_path: 交易配置文件路径
+
+    Raises:
+        Exception: 文件不存在
+    """
     if not Path(dotenv_path).exists() and not os.getenv("check_env_exist"):
         raise Exception(".env not exists")
-    if not Path(os.getenv("CONFIG_PATH", "config.yaml")).exists():
+    if not Path(os.getenv("CONFIG_PATH", config_path)).exists():
         raise Exception("config.yaml not exists")
-    if not Path(os.getenv("TRADING_CONFIG_PATH", "trading_config.yaml")).exists():
+    if not Path(os.getenv("TRADING_CONFIG_PATH", trading_path)).exists():
         raise Exception("trading_yaml not exists")
+
+def load_all_configs(
+        dotenv_path: str=".env", 
+        config_path: str="config.yaml", 
+        trading_path: str="trading_config.yaml"
+    ) -> Tuple[Env_config, Config, Trading_config]:
+    """
+    获取 env, config, trading_config 三个配置对象
+
+    Returns:
+        env_config: 环境变量配置对象
+        config: 配置对象
+        trading_config: 交易配置对象
+    """
+    # 检查 env, config, trading_config 是否存在
+    check_file_exist(dotenv_path, config_path, trading_path)
     
     env_config: Env_config = load_env_config(dotenv_path)
     config: Config = load_config()
